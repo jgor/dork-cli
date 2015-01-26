45e64eac76367e02f129291af83cf2c9ba1348b6
jgor <jgor@utexas.edu>
2015-01-26 11:26:23 -0600

    move editable settings to the top, fix num_results

--- dork-cli.py
+++ dork-cli.py
@@ -4,19 +4,27 @@ import urllib
 import json
 import sys
 
-num_results = 50
-extensions = ['asp', 'aspx', 'cfm', 'cgi', 'jsp', 'php', 'phtm', 'phtml', 'shtm', 'shtml']
+api_key = 'YOUR_GOOGLE_API_KEY_HERE'
+engine_id = 'YOUR_CUSTOM_SEARCH_ENGINE_ID_HERE'
+num_results = 30
+dynamic_extensions = ['asp', 'aspx', 'cfm', 'cgi', 'jsp', 'php', 'phtm', 'phtml', 'shtm', 'shtml']
 
 data = {}
-data['key'] = 'YOUR_KEY_HERE'
-data['cx'] = 'YOUR_ENGINE_ID_HERE'
-data['q'] = 'filetype:' + ' OR filetype:'.join(extensions) + ' ' + ' '.join(sys.argv[1:])
+data['key'] = api_key
+data['cx'] = engine_id
+data['q'] = 'filetype:' + ' OR filetype:'.join(dynamic_extensions) + ' ' + ' '.join(sys.argv[1:])
 data['num'] = 10
 data['start'] = 1
 
-while data['start'] + data['num'] <= num_results + 1:
+while data['start'] <= num_results:
+    if num_results - data['start'] + 1 < data['num']:
+        data['num'] = num_results - data['start'] + 1
     url = 'https://www.googleapis.com/customsearch/v1?'+ urllib.urlencode(data)
-    response = json.load(urllib2.urlopen(url))
+    try:
+        response = json.load(urllib2.urlopen(url))
+    except urllib2.HTTPError:
+        print 'HTTP error'
+        sys.exit(1)
     for item in response['items']:
         print item['link']
     data['start'] += data['num']
