44566b4bcc04c5d2891fe791d157c6566463dfd2
jgor <jgor@utexas.edu>
2015-02-24 10:47:20 -0600

    add main function wrapper

--- dork-cli.py
+++ dork-cli.py
@@ -18,27 +18,31 @@ data['q'] = 'filetype:' + ' OR filetype:'.join(dynamic_extensions) + ' ' + ' '.j
 data['num'] = 10
 data['start'] = 1
 
-while data['start'] <= num_results:
-    if num_results - data['start'] + 1 < data['num']:
-        data['num'] = num_results - data['start'] + 1
-    url = 'https://www.googleapis.com/customsearch/v1?'+ urllib.urlencode(data)
-    try:
-        response = json.load(urllib2.urlopen(url))
-    except urllib2.HTTPError, e:
-        response = json.load(e)
-        print >> sys.stderr, "error: " + str(response['error']['code']) + " - " + response['error']['message']
-        for error in response['error']['errors']:
-            print >> sys.stderr, error['domain'] + "::" + error['reason'] + "::" + error['message']
-        if "Exceeded" in response['error']['message']:
-            print >> sys.stderr, "sleeping " + str(sleep_secs_on_daily_limit_hit) + " seconds"
-            time.sleep(sleep_secs_on_daily_limit_hit)
-            continue
-        else:
-            sys.exit(1)
-    for request in response['queries']['request']:
-        if int(request['totalResults']) == 0:
-            sys.exit(0)
-    for item in response['items']:
-        print item['link']
-    data['start'] += data['num']
+def main():
+    while data['start'] <= num_results:
+        if num_results - data['start'] + 1 < data['num']:
+            data['num'] = num_results - data['start'] + 1
+        url = 'https://www.googleapis.com/customsearch/v1?'+ urllib.urlencode(data)
+        try:
+            response = json.load(urllib2.urlopen(url))
+        except urllib2.HTTPError, e:
+            response = json.load(e)
+            print >> sys.stderr, "error: " + str(response['error']['code']) + " - " + response['error']['message']
+            for error in response['error']['errors']:
+                print >> sys.stderr, error['domain'] + "::" + error['reason'] + "::" + error['message']
+            if "Exceeded" in response['error']['message']:
+                print >> sys.stderr, "sleeping " + str(sleep_secs_on_daily_limit_hit) + " seconds"
+                time.sleep(sleep_secs_on_daily_limit_hit)
+                continue
+            else:
+                sys.exit(1)
+        for request in response['queries']['request']:
+            if int(request['totalResults']) == 0:
+                sys.exit(0)
+        for item in response['items']:
+            print item['link']
+        data['start'] += data['num']
+
+if __name__ == "__main__":
+    main()
 
